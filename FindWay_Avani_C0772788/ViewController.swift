8e61f0013d7f03e90b642f01dd10c32893d72a4b
Avani Patel <anpatel36@yahoo.in>
2020-06-11 14:12:34 -0400

    [AP] location manager delegate added

--- FindWay_Avani_C0772788/ViewController.swift
+++ FindWay_Avani_C0772788/ViewController.swift
@@ -7,14 +7,38 @@
 //
 
 import UIKit
+import MapKit
+import CoreLocation
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var mapView: MKMapView!
+    
+    let locationManager = CLLocationManager()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    func checkLocationServices() {
+        if CLLocationManager.locationServicesEnabled() {
+            // setup location manager
+        }
+        else {
+            // Show alert letting the user know they have to turn this on.
+            
+        }
+    }
 }
 
+extension ViewController: CLLocationManagerDelegate
+{
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        <#code#>
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        <#code#>
+    }
+}
