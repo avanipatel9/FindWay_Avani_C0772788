6a4802f7ed18529d6447f1099a856d805283dda9
Avani Patel <anpatel36@yahoo.in>
2020-06-11 16:57:29 -0400

    [AP] Pin added to user location

--- FindWay_Avani_C0772788/ViewController.swift
+++ FindWay_Avani_C0772788/ViewController.swift
@@ -32,6 +32,10 @@ class ViewController: UIViewController {
         if let location = locationManager.location?.coordinate {
             let region = MKCoordinateRegion.init(center: location, latitudinalMeters: regionInMeters, longitudinalMeters: regionInMeters)
             mapView.setRegion(region, animated: true)
+            
+            let pin = MKPointAnnotation()
+            pin.coordinate = CLLocationCoordinate2D(latitude: location.latitude, longitude: location.longitude)
+            mapView.addAnnotation(pin)
         }
     }
     
@@ -47,6 +51,7 @@ class ViewController: UIViewController {
     }
     
     func checkLocationAuthorization() {
+        
         switch CLLocationManager.authorizationStatus() {
         case .authorizedWhenInUse:
             centerViewOnUserLocation()
@@ -71,13 +76,18 @@ class ViewController: UIViewController {
 extension ViewController: CLLocationManagerDelegate
 {
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        
         guard let location = locations.last else { return }
+        
         let center = CLLocationCoordinate2D(latitude: location.coordinate.latitude, longitude: location.coordinate.longitude)
+        
         let region = MKCoordinateRegion.init(center: center, latitudinalMeters: regionInMeters, longitudinalMeters: regionInMeters)
+        
         mapView.setRegion(region, animated: true)
     }
     
     func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        
         checkLocationAuthorization()
     }
 }
