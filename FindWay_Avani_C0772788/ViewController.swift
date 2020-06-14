b47e83c9f9d7765ec428e3f49aafe4c66ba169c7
Avani Patel <anpatel36@yahoo.in>
2020-06-14 02:10:02 -0400

    [AP] Displayed route, polyline method, button added for zoom in and zoom out

--- FindWay_Avani_C0772788/ViewController.swift
+++ FindWay_Avani_C0772788/ViewController.swift
@@ -13,16 +13,19 @@ import CoreLocation
 class ViewController: UIViewController {
 
     @IBOutlet weak var mapView: MKMapView!
-    @IBOutlet weak var addressLabel: UILabel!
+//    @IBOutlet weak var addressLabel: UILabel!
     
     let locationManager = CLLocationManager()
     let regionInMeters: Double = 10000
     var previousLocation: CLLocation?
+    var destination: CLLocationCoordinate2D!
+     var travelMode: String = "Drive"
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         checkLocationServices()
+        addDoubleTap()
     }
     
     func setupLocationManager() {
@@ -85,20 +88,112 @@ class ViewController: UIViewController {
         
         return CLLocation(latitude: latitude, longitude: longitude)
     }
+    
+    
+    @IBAction func travelModeSegment(_ sender: UISegmentedControl) {
+        let overlays = mapView.overlays
+        mapView.removeOverlays(overlays)
+        
+        if sender.selectedSegmentIndex == 0 {
+            travelMode = "D"
+        }
+        else{
+            travelMode = "W"
+        }
+    }
+    
+    @IBAction func drawLocationBtnClick(_ sender: UIButton) {
+        let overlays = mapView.overlays
+        mapView.removeOverlays(overlays)
+        //draw route
+        let sourcePlacemark = MKPlacemark(coordinate: locationManager.location!.coordinate)
+        let destinationPlacemark = MKPlacemark(coordinate: destination)
+        
+        let directionRequest = MKDirections.Request()
+        directionRequest.source = MKMapItem(placemark: sourcePlacemark)
+        directionRequest.destination = MKMapItem(placemark: destinationPlacemark)
+        
+        directionRequest.requestsAlternateRoutes = true
+        
+        if(travelMode == "D") {
+            directionRequest.transportType = .automobile
+        }
+        else {
+            directionRequest.transportType = .walking
+        }
+        
+        let directions = MKDirections(request: directionRequest)
+        directions.calculate { (response, error) in
+            guard let unwrappedResponse = response else { return }
+            let route = unwrappedResponse.routes[0]
+            self.mapView.addOverlay(route.polyline, level: .aboveRoads)
+            self.mapView.setVisibleMapRect(route.polyline.boundingMapRect, animated: true)
+                
+                let distance = route.distance
+                
+                let alert = UIAlertController(title: "Let's go", message: "Distnace : \(distance) KM", preferredStyle: .alert)
+                       let cancelAction = UIAlertAction(title: "OK", style: .cancel, handler: nil)
+                       alert.addAction(cancelAction)
+                self.present(alert, animated: true, completion: nil)
+        }
+    }
+    
+    @IBAction func zoomInBtnClick(_ sender: UIButton) {
+        let span = MKCoordinateSpan(latitudeDelta: mapView.region.span.latitudeDelta/2, longitudeDelta: mapView.region.span.longitudeDelta/2)
+        let region = MKCoordinateRegion(center: mapView.region.center, span: span)
+        mapView.setRegion(region, animated: true)
+    }
+    
+    @IBAction func zoomOutBtnClick(_ sender: UIButton) {
+        let span = MKCoordinateSpan(latitudeDelta: mapView.region.span.latitudeDelta*2, longitudeDelta: mapView.region.span.longitudeDelta*2)
+        let region = MKCoordinateRegion(center: mapView.region.center, span: span)
+        mapView.setRegion(region, animated: true)
+    }
+    
+    
 }
 
 extension ViewController: CLLocationManagerDelegate
 {
-//    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
-//
-//        guard let location = locations.last else { return }
-//
-//        let center = CLLocationCoordinate2D(latitude: location.coordinate.latitude, longitude: location.coordinate.longitude)
-//
-//        let region = MKCoordinateRegion.init(center: center, latitudinalMeters: regionInMeters, longitudinalMeters: regionInMeters)
-//
-//        mapView.setRegion(region, animated: true)
-//    }
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+
+        guard let location = locations.last else { return }
+
+        let center = CLLocationCoordinate2D(latitude: location.coordinate.latitude, longitude: location.coordinate.longitude)
+
+        let region = MKCoordinateRegion.init(center: center, latitudinalMeters: regionInMeters, longitudinalMeters: regionInMeters)
+
+        mapView.setRegion(region, animated: true)
+    }
+    
+    func addDoubleTap() {
+           let doubleTap = UITapGestureRecognizer(target: self, action: #selector(dropPin))
+           doubleTap.numberOfTapsRequired = 2
+           mapView.addGestureRecognizer(doubleTap)
+       }
+    
+    @objc func dropPin(sender: UITapGestureRecognizer) {
+        removePin()
+        
+        // add annotation
+        
+        let touchPoint = sender.location(in: mapView)
+        let coordinate = mapView.convert(touchPoint, toCoordinateFrom: mapView)
+        let annotation = MKPointAnnotation()
+        annotation.title = "My destination"
+        annotation.coordinate = coordinate
+        mapView.addAnnotation(annotation)
+        
+        destination = coordinate
+    }
+    
+    func removePin() {
+            for annotation in mapView.annotations {
+                mapView.removeAnnotation(annotation)
+            }
+        }
+    
+    
     
     func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
         
@@ -107,38 +202,79 @@ extension ViewController: CLLocationManagerDelegate
 }
 
 extension ViewController: MKMapViewDelegate {
-    func mapView(_ mapView: MKMapView, regionDidChangeAnimated animated: Bool) {
-        
-        let center = getCenterLocation(for: mapView)
-        
-        let geoCoder = CLGeocoder()
-        
-        guard let previousLocation = self.previousLocation else { return }
-        
-        guard center.distance(from: previousLocation) > 50 else { return }
-        self.previousLocation = center
-        
-        geoCoder.reverseGeocodeLocation(center) { [weak self] (placemarks, error) in
-        guard let self = self else { return }
-        
-        if let _ = error {
-            //TODO: Show alert informing the user
-            return
-        }
-        
-        guard let placemark = placemarks?.first else {
-            //TODO: Show alert informing the user
-            return
-        }
-        
-        let streetNumber = placemark.subThoroughfare ?? ""
-        let streetName = placemark.thoroughfare ?? ""
-        
-        DispatchQueue.main.async {
-            self.addressLabel.text = "\(streetNumber) \(streetName)"
-        }
+    
+    //MARK: - add viewFor annotation method
+       func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+           
+           if annotation is MKUserLocation {
+               return nil
+           }
+           // add custom annotation with image
+               let pinAnnotation = mapView.dequeueReusableAnnotationView(withIdentifier: "droppablePin") ?? MKPinAnnotationView()
+               pinAnnotation.image = UIImage(systemName: "mappin")
+               pinAnnotation.canShowCallout = true
+               pinAnnotation.rightCalloutAccessoryView = UIButton(type: .detailDisclosure)
+               return pinAnnotation
+           }
+    //MARK: - callout accessory control tapped
+    func mapView(_ mapView: MKMapView, annotationView view: MKAnnotationView, calloutAccessoryControlTapped control: UIControl) {
+        let alertController = UIAlertController(title: "Your Place", message: "Welcome", preferredStyle: .alert)
+        let cancelAction = UIAlertAction(title: "OK", style: .cancel, handler: nil)
+        alertController.addAction(cancelAction)
+        present(alertController, animated: true, completion: nil)
+    }
+    
+    //MARK: - Render for overlay
+    func mapView(_ mapView: MKMapView, rendererFor overlay: MKOverlay) -> MKOverlayRenderer {
+        if overlay is MKPolyline {
+            let rendrer = MKPolylineRenderer(overlay: overlay)
+            if(travelMode == "D")
+            {
+                rendrer.strokeColor = UIColor.red
+            }
+            else
+            {
+                rendrer.strokeColor = UIColor.blue
+                rendrer.lineDashPattern = [0,4]
+            }
+            rendrer.lineWidth = 3
+            return rendrer
             
         }
-        
+        return MKOverlayRenderer()
     }
+    
+    
+//    func mapView(_ mapView: MKMapView, regionDidChangeAnimated animated: Bool) {
+//        
+//        let center = getCenterLocation(for: mapView)
+//        
+//        let geoCoder = CLGeocoder()
+//        
+//        guard let previousLocation = self.previousLocation else { return }
+//        
+//        guard center.distance(from: previousLocation) > 50 else { return }
+//        self.previousLocation = center
+//        
+//        geoCoder.reverseGeocodeLocation(center) { [weak self] (placemarks, error) in
+//        guard let self = self else { return }
+//        
+//        if let _ = error {
+//            //TODO: Show alert informing the user
+//            return
+//        }
+//        guard let placemark = placemarks?.first else {
+//            //TODO: Show alert informing the user
+//            return
+//        }
+//        let streetNumber = placemark.subThoroughfare ?? ""
+//        let streetName = placemark.thoroughfare ?? ""
+//        let subLocality = placemark.subLocality ?? ""
+//        let postalCode = placemark.postalCode ?? ""
+//        
+//        DispatchQueue.main.async {
+//            self.addressLabel.text = "\(streetNumber) \(streetName) \(subLocality) \(postalCode)"
+//        }
+//    }
+//    }
 }
